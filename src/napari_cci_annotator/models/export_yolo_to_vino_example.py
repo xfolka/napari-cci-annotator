4c07492c0e7b4781188fa122a69f8cdec1f39efa
Anders Folkesson <anders.folkesson@gu.se>
2025-04-25 15:12:30 +0200

    added axon models

---
+++ src/napari_cci_annotator/models/export_yolo_to_vino_example.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a YOLOv8n PyTorch model
+model = YOLO("latest_model.pt")
+
+# Export the model
+model.export(format="openvino")  # creates 'yolov8n_openvino_model/'
+
+#REFERENCE: https://docs.ultralytics.com/integrations/openvino/#how-do-i-export-yolov8-models-to-openvino-format
