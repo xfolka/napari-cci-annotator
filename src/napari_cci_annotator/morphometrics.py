fixed calculation for morphotemtrics

src/napari_cci_annotator/morphometrics.py
@@ -12,6 +12,8 @@ import pandas as pd
 from skimage.color import label2rgb
 import numpy as np
 import xlsxwriter
+import copy
+from .segment_large_image_using_yolo import IntGenerator
 
 
 from scipy.ndimage import center_of_mass
@@ -37,15 +39,12 @@ def bbox_from_BWimg(BW):
 def get_box_with_rotation(bw_img, rot_mat):
     chull_img = convex_hull_image(bw_img)
     chull_contour = find_contours(chull_img)
-    #assert len(chull_contour)==1, "we only expect one contour"
     contours = len(chull_contour)
     if contours!=1:
         return contours, None, None 
     convex_hull = morpho.smoothBoundary(chull_contour[0])
     r_hull = morpho.rotate_points(convex_hull, rot_mat)
     box_dim = morpho.bbox_dimenssions(r_hull)
-
-    #print(box_dim)
 
     min_bbox_points = np.array([
         [0, 0],
@@ -60,33 +59,10 @@ def get_box_with_rotation(bw_img, rot_mat):
     min_bbox_corners = min_bbox_corners - np.mean(min_bbox_corners, axis=0)
     #   move the box to the same reference as the inpurt convex hull
     c_mass = center_of_mass(chull_img, labels=None, index=None)
-    min_bbox_corners = min_bbox_corners + c_mass #((np.max(convex_hull, axis=0)-np.min(convex_hull, axis=0))/2)+np.min(convex_hull, axis=0)
+    min_bbox_corners = min_bbox_corners + c_mass
 
     return contours, min_bbox_corners, box_dim
 
-
-# def get_morphos_for_label(label,label_image, data_image):
-#     lbl = np.where(label_image == label, label, 0)
-#     properties = ['bbox','image']
-#     reg_table = regionprops_table(label_image=label_image,
-#                             properties=properties)
-    
-#     if len(reg_table) == 0:
-#         return None
-    
-#     row = reg_table[0]
-#     min_r = row['bbox-0']
-#     min_c = row['bbox-1']
-#     max_r = row['bbox-2']
-#     max_c = row['bbox-3']
-
-#     myelin_lbl = label_image[min_r:max_r,min_c:max_c]
-#     myelin_bw = row['image']
-#     img_data = data_image[min_r:max_r,min_c:max_c]
-    
-#     return get_morphos_for_myelin_label_data(myelin_lbl,myelin_bw,img_data,label)
-   
-   
 def get_label_for_coord(label_image, x, y):
     if label_image is None:
         return 0
@@ -127,31 +103,17 @@ def morpho_data_generator(myelin_label_image, axon_label_image, data_image):
     print(f"Number of labels in layer: {len(reg_table['label'])} or {num_labels}")
     
     reg_table = pd.DataFrame(reg_table)
-    #reg_table.sample(5)
-
-    #openCnt = 0
     totCnt = 0
+    axon_max_label = np.max(axon_label_image)
 
-
-#with wk.webknossos_context(token=AUTH_TOKEN, timeout=WK_TIMEOUT):
     for index, row in reg_table.iterrows():
-        #print(row)
-        #obj_idx = row['label']
-        totCnt += 1
-    # bbox = skibbox2wkbbox(row.to_dict(), pSize)
-    # img_data = img_layer.get_finest_mag().read(absolute_offset=bbox.topleft, size=bbox.size)
-
-    # myelin_lbl = lbl_layers[myelin_idx].get_finest_mag().read(absolute_offset=bbox.topleft, size=bbox.size).squeeze()
-    # axon_lbl   = lbl_layers[axon_idx].get_finest_mag().read(absolute_offset=bbox.topleft, size=bbox.size).squeeze()
-    # mito_lbl   = lbl_layers[mito_idx].get_finest_mag().read(absolute_offset=bbox.topleft, size=bbox.size).squeeze()
-    # dyst_lbl   = lbl_layers[dystrophic_idx].get_finest_mag().read(absolute_offset=bbox.topleft, size=bbox.size).squeeze()
 
         min_r = row['bbox-0']
         min_c = row['bbox-1']
         max_r = row['bbox-2']
         max_c = row['bbox-3']
 
-        myelin_lbl = myelin_label_image[min_r:max_r,min_c:max_c]
+        myelin_lbl = copy.copy(myelin_label_image[min_r:max_r,min_c:max_c])
         myelin_bw = row['image']
         img_data = data_image[min_r:max_r,min_c:max_c]
         
@@ -162,12 +124,13 @@ def morpho_data_generator(myelin_label_image, axon_label_image, data_image):
         axon_bw_crop = None
         axon_lbl_crop = None
         if axon_label != 0:
-            axon_lbl_crop = axon_label_image[min_r:max_r,min_c:max_c]
+            axon_lbl_crop = copy.copy(axon_label_image[min_r:max_r,min_c:max_c])
             axon_bw_crop = (axon_lbl_crop == axon_label).astype(bool)
             axon_lbl_crop = axon_lbl_crop * axon_bw_crop
-            #axon_lbl_crop, axon_bw_crop = extract_labeled_region(axon_label_image, axon_label) #type: ignore
-        
-        yield get_morphos_for_label_data(myelin_lbl,myelin_bw, axon_lbl_crop, axon_bw_crop, img_data, min_r, min_c)
+
+        print(f"calculated {totCnt}:th morphometric!")
+        totCnt += 1        
+        yield get_morphos_for_label_data(myelin_lbl,myelin_bw, axon_lbl_crop, axon_bw_crop, img_data, axon_max_label, min_r, min_c)
         
         
 def auto_calculate_axon_region(myelin_bw_fill, myelin_bw):
@@ -181,21 +144,15 @@ def auto_calculate_axon_region(myelin_bw_fill, myelin_bw):
     auto_axon_region = np.logical_xor(myelin_bw_fill,myelin_bw)
     auto_axon_region = isotropic_erosion(auto_axon_region, 3)
     auto_axon_region = remove_small_objects(auto_axon_region, min_size=500)
-    #assert np.sum(auto_axon_region)>500, f"unexpected problems with the auto axon region {obj_idx}"
-    print("before axon sum...")
     if np.sum(auto_axon_region)<500:
-        print("after axon sum...")
+        #print("after axon sum...")
         return None
 
     return auto_axon_region
     
-def get_morphos_for_label_data(myelin_lbl, myelin_bw, axon_lbl, axon_bw, img_data, x_offset = 0, y_offset = 0):
+def get_morphos_for_label_data(myelin_lbl, myelin_bw, axon_lbl, axon_bw, img_data, axon_max_label, x_offset = 0, y_offset = 0):
     
     pad_size = 5
-
-    # get area of the imge identified as myelin, this is my main ROI which defines behaviour of all others.
-#        myelin_bw = morpho.get_BW_from_lbl(myelin_lbl, obj_idx)
-    # clean myelin label map, in case other neurons are close by
     myelin_lbl[np.logical_not(myelin_bw)] = 0
 
     properties = ['label','area','eccentricity','solidity','intensity_mean',
@@ -204,7 +161,8 @@ def get_morphos_for_label_data(myelin_lbl, myelin_bw, axon_lbl, axon_bw, img_dat
                                     intensity_image=img_data,
                                     properties=properties)
     obj_table = pd.DataFrame(props_table)
-    obj_table.rename(columns={'area': 'myelin_area', 
+    obj_table.rename(columns={'label' : 'myelin_label',
+                        'area': 'myelin_area', 
                         'eccentricity': 'myelin_eccentricity',
                         'solidity': 'myelin_solidity',
                         'intensity_mean': 'myelin_intensity_mean',
@@ -215,7 +173,6 @@ def get_morphos_for_label_data(myelin_lbl, myelin_bw, axon_lbl, axon_bw, img_dat
     obj_table['nr_of_parts'] = 1
     obj_table['status_ok'] = True
     obj_table['error_msg'] = ""
-    ##assert len(obj_table.axes[0]) == 1, "we expected to have a single object"
     if len(obj_table.axes[0]) > 1:
         obj_table['nr_of_parts'] = obj_table.axes[0]
         obj_table['status_ok'] = False
@@ -236,7 +193,7 @@ def get_morphos_for_label_data(myelin_lbl, myelin_bw, axon_lbl, axon_bw, img_dat
         axon_region = auto_calculate_axon_region(myelin_bw_fill,myelin_bw)
         #debug = True
         obj_table['axon_calculation'] = "Automatic"
-        print("no axon here")
+        #print("no axon here")
     else:
         obj_table['axon_calculation'] = "Annotation"
         axon_region = axon_bw
@@ -250,7 +207,7 @@ def get_morphos_for_label_data(myelin_lbl, myelin_bw, axon_lbl, axon_bw, img_dat
         
         
     else:
-        axon_table = get_morphos_for_axon_label_data(axon_region,obj_table,pad_size, debug)
+        axon_table = get_morphos_for_axon_label_data(axon_region,axon_lbl,obj_table, axon_max_label,pad_size)
         
     obj_table['myelin_hole_area'] = obj_table['axon_area']
         
@@ -271,9 +228,6 @@ def get_morphos_for_label_data(myelin_lbl, myelin_bw, axon_lbl, axon_bw, img_dat
         obj_table['status_ok'] = False
         obj_table['error_msg'] = str(e)
         return obj_table
-
-    # auto_axon_area = np.sum(axon_region)
-    # obj_table['myelin_hole_area'] =auto_axon_area
 
     if axon_region is None:
         obj_table['myelin_width_min_feret_direction'] = "skipped"
@@ -298,26 +252,31 @@ def get_morphos_for_label_data(myelin_lbl, myelin_bw, axon_lbl, axon_bw, img_dat
     
     return obj_table
 
-def get_morphos_for_axon_label_data(axon_bw, obj_table, pad_size = 5, debug = False):
+axon_id_generator = IntGenerator()
+
+def get_morphos_for_axon_label_data(axon_bw_img, axon_lbl_img, obj_table, axon_max_label, pad_size = 5, debug = False):
 
     # # I do not expect holes in the axon image
-    if debug: print("a")
-    axon_bw = ndimage.binary_fill_holes(axon_bw)
-    if debug: print("b")
-    axon_bw_pad = np.pad(axon_bw, ((pad_size, pad_size), (pad_size, pad_size),), mode='constant', constant_values=0)
+    if axon_lbl_img is None:
+        axon_id_generator.setStartValue(axon_max_label)
+        axon_lbl = axon_id_generator.getNext()
+    else:
+        properties = ['label']
+        props_table = regionprops_table(label_image=axon_lbl_img,
+                                    properties=properties)
+        axon_lbl = props_table['label']
+    
+    obj_table['axon_label'] = axon_lbl
+    axon_bw_img_tmp = ndimage.binary_fill_holes(axon_bw_img)
+    axon_bw_pad = np.pad(axon_bw_img_tmp, ((pad_size, pad_size), (pad_size, pad_size),), mode='constant', constant_values=0)
 
     # # convex hull calculation including smoothing to avoid pixelation effects
-    if debug: print("1")
     axon_min_box_corners, axon_min_box_edges, _ = bbox_from_BWimg(axon_bw_pad)
 
     # # using the bbox as feret calculator and AR
-    if debug: print("2")
     obj_table['axon_area'] = np.sum(axon_bw_pad)
-    if debug: print("3")
     obj_table['axon_feret_max'] = np.max(axon_min_box_edges)
-    if debug: print("4")
     obj_table['axon_feret_min'] = np.min(axon_min_box_edges)
-    if debug: print("5")
     obj_table['axon_AR'] = np.max(axon_min_box_edges)/np.min(axon_min_box_edges)
 
     return obj_table
@@ -333,8 +292,5 @@ def create_morpho_table_from_data(name, myelin_label_image, axon_label_image, da
         total_table = pd.concat([total_table, data], axis=0)
         tot_cnt += 1
         
-
-#    print(f"Morphonetrics done. Tot: {tot_cnt}")
-
     total_table.to_excel(name, index=False, engine='xlsxwriter')
 